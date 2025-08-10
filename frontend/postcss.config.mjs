d57b7d3228beefbaa14038b1b527ed12ca027399
lukbrinker <lukbrinker@gmail.com>
2025-08-09 22:17:44 -0700

    chore: resolve rebase

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -6,20 +6,4 @@ const config = {
 };
 
 export default config;
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-    autoprefixer: {},
->>>>>>> Stashed changes
-=======
-    autoprefixer: {},
->>>>>>> Stashed changes
-  },
-};
-
-export default config;
 
